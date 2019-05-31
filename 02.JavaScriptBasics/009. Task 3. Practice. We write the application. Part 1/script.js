d3f363778d9d73f21ecef172860eb10edbe7f33c
pandrey76 <pandrey76@yandex.ru>
2019-05-31 10:03:07 +0300

    Viewed 009
    Просмотренно видео "009. Задание 2. Задачи на понимание основ JS
    (с реальных собеседований)", "009 Циклы" и "009. Задание 3. Практика.
    Пишем приложение. Часть 1"!

--- 02.JavaScriptBasics/009. Task 3. Practice. We write the application. Part 1/script.js
+++ 02.JavaScriptBasics/009. Task 3. Practice. We write the application. Part 1/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money = +prompt("Ваш бюджет на месяц?", ''),    // Должны быть введены цифры, если не ввести, 
+                                                    // то в консоль выведется сообщение "Произошла ошибка"
     time = prompt("Введите дату в формате YYYY-VV-DD", "");
 
 let appData = {
@@ -12,13 +13,37 @@ let appData = {
     saving: false,
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", "");
-    a2 = prompt("Во сколько обойдётся?", "");
-    a3 = prompt("Введите обязательную статью расходов в этом месяце", "");
-    a4 = prompt("Во сколько обойдётся?", "");
-    
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдётся?", "");
 
-alert(appData.budget /30);
+    // Если в prompt нажать отмена, то мы получим "null" 
+    if ( (typeof(a) === 'string')  &&   // Должна быть строка
+         (typeof(a) != null) &&         // Нельзя нажимать отмену в первом окне
+         (typeof(b) != null) &&         // Нельзя нажимать отмену во втором окне   
+         (b != '') &&                   // Обязательно необходимо ввести данные в первое окно
+         (a != '') &&                   // Обязательно необходимо ввести данные во второе окно
+         (a.length < 50)
+         ) {        
+            console.log("done");
+            appData.expenses[a] = b;
+    }
+    else {
+
+    }
+ };
+
+ appData.moneyPerDay = appData.budget / 30;
+
+ alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+ if(appData.moneyPerDay < 100) {
+     console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+     console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000 ) {
+     console.log("Высокий уровень достатка");
+} else {
+     console.log("Произошла ошибка");  
+}
 
